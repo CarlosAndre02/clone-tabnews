30ed5e1f7157ffcd562f2dfffc867a7e40d71460
Carlos André <dev.carlosandre@gmail.com>
2025-08-25 10:00:57 -0300

    curso: dia 18

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
-export function GET(req) {
+export async function GET(req) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
   return NextResponse.json({ chave: "são acima da média" });
 }
